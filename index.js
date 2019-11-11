965fbe5dd3a236e3b4200f673aacabf4e938f122
Diana Badas <diana.badas@gmail.com>
2019-11-11 16:05:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-// Write your solution in this file!
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  const newObj = {...driver};
-
-  newObj[key] = value;
-
-  return newObj;
-};
+  // Alternate using ES6 Spread operators:
+  // return { ...driver, ...{ [key]: value } }
+  return Object.assign({}, driver, { [key]: value });
+}
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
 
   return driver;
-};
+}
 
 function deleteFromDriverByKey(driver, key) {
-  const newDriver = { ...driver };
-  delete newDriver[key];
-  return newDriver;
-};
+  // Alternate using ES6 Spread operators:
+  // const newObj = { ...driver }
+  const newObj = Object.assign({}, driver);
+
+  delete newObj[key];
+
+  return newObj;
+}
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver[key];
+
   return driver;
-};
+}
