fc14bdd8826a6124208783eee73a6ca43eaacc5e
github username <github email address>
2020-09-16 17:16:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const driver = {}
+
+//returns a driver w the original key value pairs and new key value pair
+//not modify original driver 
+function updateDriverWithKeyAndValue(driver, key, value){
+    //Object.assign() combines properties from multiple objects into  a single object 
+    return Object.assign({}, driver, { [key]: value})
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+    let newObj = Object.assign({}, driver)
+    delete newObj[key];
+    return newObj;
+  }
+
+
+  function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key];
+    return driver
+  }
