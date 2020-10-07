a9b3eb6f4e3052e9c341a29142129de96c1bca5f
rohanphillips <rohan@solutions-insurance.com>
2020-10-07 13:26:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let driver = {};
+
+function updateDriverWithKeyAndValue(object, key, value){
+  let myClone = Object.assign({}, object);
+  myClone[key] = value;
+  return myClone;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromDriverByKey(object, key){
+  let myClone = Object.assign({}, object);
+  delete myClone[key];
+  return myClone;
+}
+
+function destructivelyDeleteFromDriverByKey(object, key){
+  delete object[key];
+  return object;
+}
+
+
+let myUpdate = updateDriverWithKeyAndValue(driver, "Name", "Rohan");
+deleteFromDriverByKey(myUpdate, "Name")
