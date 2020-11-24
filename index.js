3973bf069872b1708b3c130035cc0ca8ea03b7db
Yumacorn <ReachRyanMui@gmail.com>
2020-11-24 00:15:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let driver = {};
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+    return Object.assign({}, obj, {[key]: value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromDriverByKey(driver, key) {
+    let copyObject = driver;
+    console.log("QUick");
+    console.log(driver);
+    delete copyObject[key];
+    return driver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+
+}
