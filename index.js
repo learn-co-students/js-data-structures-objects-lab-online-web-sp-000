f63e52e0d294cfcf2af8563cf10c39f9d2893acb
catprice444 <catprice444@gmail.com>
2021-03-27 18:29:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+
+
+// - `destructivelyDeleteFromDriverByKey()` - this function should work the same as
+//   `deleteFromDriverByKey()` but it _should_ mutate the `driver` passed in. Be
+//   sure and consider whether dot-notation or bracket-notation might affect your
+//   solution.
+
+
+
+
+
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    return Object.assign({}, driver, { [key]: value });
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+    return driver;
+};
+
+function deleteFromDriverByKey(driver, key){
+    const newDriver = Object.assign({}, driver);
+    delete newDriver[key];
+    return newDriver
+};
+
+function destructivelyDeleteFromDriverByKey (driver, key){
+    delete driver[key];
+    return driver
+};
+
